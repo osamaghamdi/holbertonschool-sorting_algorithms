67e03f9257af75a5f9da565b5fd048be5b768b2e
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-09 13:47:43 +0300

    Create 0-bubble_sort.c
    create and add logic code for bubble sort.

---
+++ 0-bubble_sort.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    int i, j, temp;
+
+    for (i = 0; i < size; i++)
+    {
+      for (j = 0; j < size - 1; j++)
+      {
+        if (array[j] > array[j + 1])
+        {
+          temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          print_array(array, size);
+        }
+      }
+    }
+}
