fa9170af3263fdbd2f27eda39572214b5deedfdb
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-10 13:44:12 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,17 +9,13 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
+	int i, j, temp;
 
-	if (!array || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	/*Loop through the array 'size' times*/
+	for (i=0 ; i < size ; i++)
 	{
-		swapped = 0;
-		for (j = 0; j < size - 1 - i; j++)
+		/*Compare each pair of adjacent elements*/
+		for (j=0 ; j < size - 1 ; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -27,10 +23,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-				swapped = 1;
 			}
 		}
-		if (!swapped)
-			break;
 	}
 }
